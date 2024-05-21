0db1b76c849e4b72c002ab582ad2ac271758c20a
andrshab <andrshab0@gmail.com>
2024-05-21 16:21:57 +0300

    remove comments

--- server/main.cpp
+++ server/main.cpp
@@ -7,18 +7,15 @@
 #include "controller/controller.hpp"
 #include "misc/globals.hpp"
 
-// declare the extern server
 Server server;
 
 int main(int argc, char **argv)
 {
-	// set server callbacks
 	server.onConnect(&Controller::handleServerConnection);
 	server.onDisconnect(&Controller::handleServerDisconnect);
 	server.onInput(&Controller::handleServerInput);
 	server.init();
 
-	// actual main loop
 	while (true)
 	{
 		server.loop();
